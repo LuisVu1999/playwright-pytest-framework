3b487bbc1b1be1e251aeba9d79723be6a31acc87
quyvuduc123456@gmail.com <GaBigQa!@12>
2025-08-23 22:05:03 +0700

    rrtgf

--- tests/ui/test_abc.py
+++ tests/ui/test_abc.py
@@ -3,7 +3,7 @@ from playwright.sync_api import sync_playwright
 def test_login():
     with sync_playwright() as p:
         # Mở browser
-        browser = p.chromium.launch(headless=False)  # headless=False để thấy trình duyệt chạy
+        browser = p.chromium.launch(headless=True)  # headless=False để thấy trình duyệt chạy
         page = browser.new_page()
 
         # Truy cập trang login
