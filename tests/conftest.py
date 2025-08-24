60967bbb5cae7f174f5f51e38336dc04457ddbc9
quyvuduc123456@gmail.com <GaBigQa!@12>
2025-08-24 13:13:26 +0700

    ttt

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,16 +3,17 @@ from playwright.sync_api import sync_playwright
 
 BASE_URL = "https://demo.growcrm.io/login"
 
-@pytest.fixture(scope="session")
-def playwright_instance():
-    with sync_playwright() as p:
-        yield p
+# @pytest.fixture(scope="session")
+# def playwright_instance():
+#     with sync_playwright() as p:
+#         yield p
 
 @pytest.fixture(scope="session")
-def browser(playwright_instance):
-    browser = playwright_instance.chromium.launch(headless = True)
-    yield browser
-    browser.close()
+def browser():
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless = True)
+        yield browser
+        browser.close()
 
 @pytest.fixture(scope="function")
 def page(browser):
