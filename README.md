2a16e859ed9d6d059b2c09c88f3237f96ce62f9b
LuisVu1999 <LuisVu1999@users.noreply.github.com>
2025-08-29 17:02:04 +0000

    deploy: 99eff5e5a1bcc59de319e5b34e2049337738f74f

--- README.md
+++
@@ -1,4 +0,0 @@
-# playwright_test2
-
-#chuyen vao env: .venv\Scripts\activate
-#Xem report: allure serve allure-results
--- tests/ui/login/test_login.py
+++
@@ -1,5 +0,0 @@
-from pages.login_page import LoginPage
-
-def test_login(page):
-    login_page = LoginPage(page)
-    login_page.login("admin@example.com","growcrm")
