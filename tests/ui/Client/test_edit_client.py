a409dda395d83f9fa06ca06fd4e753da6b723961
quyvuduc123456@gmail.com <GaBigQa!@12>
2025-09-07 23:11:56 +0700

    Luis modify edit client and edit user on 9/7

--- tests/ui/Client/test_edit_client.py
+++ tests/ui/Client/test_edit_client.py
@@ -1,16 +1,16 @@
 from pages.client_page import ClientPage
 from helpers.test_data import TestData
 
-def test_edit_client(page):
+def test_edit_client(page, client_fixture):
     client_page = ClientPage(page)
-    # client_id, data = client_fixture
-    # company = data["company"]
+    client_id, data = client_fixture
+    company = data["company"]
     company_name_created = TestData.random_company()
     company_name_edited = TestData.random_company()
 
-    client_page.access_client()
-    client_page.create_client(company_name_created, TestData.random_first_name(),TestData.random_last_name(), TestData.random_email())
-    client_page.search_client(company_name_created)
+    # client_page.access_client()
+    # client_page.create_client(company_name_created, TestData.random_first_name(),TestData.random_last_name(), TestData.random_email())
+    client_page.search_client(company)
     client_page.edit_client(company_name_edited)
-    client_page.search_client(company_name_edited)
-    client_page.delete_client()
+    # client_page.search_client(company_name_edited)
+    # client_page.delete_client()
--- tests/ui/User/test_edit_user.py
+++ tests/ui/User/test_edit_user.py
@@ -2,17 +2,17 @@ from pages.login_page import LoginPage
 from pages.user_page import UserPage
 from helpers.test_data import TestData
 
-def test_edit_user(page):
+def test_edit_user(page, user_fixture):
     user_page = UserPage(page)
 
-    # user_id, data = user_fixture
-    # first_name = data["first"]
-    first_name_created = TestData.random_first_name()
+    user_id, data = user_fixture
+    first_name = data["first"]
+    # first_name_created = TestData.random_first_name()
     first_name_edited = TestData.random_first_name()
 
-    user_page.access_user()
-    user_page.create_user("Dellon Inc", first_name_created, TestData.random_last_name(), TestData.random_email(), "0976765654", "QA Engineer")
-    user_page.search_user(first_name_created)
+    # user_page.access_user()
+    # user_page.create_user("Dellon Inc", first_name_created, TestData.random_last_name(), TestData.random_email(), "0976765654", "QA Engineer")
+    user_page.search_user(first_name)
     user_page.edit_user(first_name_edited, TestData.random_last_name(), TestData.random_email(), "0976765654", "QA Engineer_modified", TestData.random_title())
-    user_page.search_user(first_name_edited)
-    user_page.delete_user()
+    # user_page.search_user(first_name_edited)
+    # user_page.delete_user()
